ade94b9da79c3e34375d7a1ce51f0d86a8f12afe
kyra-backend <ravimandrawaliya8@gmail.com>
2026-03-11 17:38:07 +0530

    Update trailercards.js

--- frontend/js/trailercards.js
+++ frontend/js/trailercards.js
@@ -2,20 +2,25 @@ const YT_THUMB = "https://img.youtube.com/vi/";
 
 function createTrailerCard(video, movie){
 
+    if(!video || !video.key) return null;
+
     const thumb = `${YT_THUMB}${video.key}/hqdefault.jpg`;
 
     const card = document.createElement("div");
     card.className = "trailer-card";
 
     card.innerHTML = `
+
         <div class="trailer-thumb">
-            <img src="${thumb}">
-            <div class="play-btn">▶</div>
+
+            <img src="${thumb}" alt="${movie.title} trailer">
+
+            <div class="play-sm">▶</div>
+
         </div>
 
-        <div class="trailer-title">
-            ${movie.title}
-        </div>
+        <p>${movie.title}</p>
+
     `;
 
     card.onclick = () => {
