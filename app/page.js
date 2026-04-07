6d83bf3138acb021ed4558c0f78dc6588cc998b4
kyra-backend <ravimandrawaliya8@gmail.com>
2026-04-07 18:33:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,23 @@
+import MovieSection from "@/components/MovieSection";
+import QuickButtons from "@/components/QuickButtons";
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <h1>Movnix Running 🚀</h1>
+    <>
+      <Header />
+
+      <QuickButtons />
+
+      <MovieSection
+        title="Trending Movies"
+        endpoint="/trending/movie/week"
+      />
+
+      <MovieSection
+        title="Popular in India"
+        endpoint="/discover/movie?with_origin_country=IN"
+      />
+    </>
   );
 }
