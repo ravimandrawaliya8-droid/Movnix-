355c6f158f74848f541e87693eba2a3488798814
kyra-backend <ravimandrawaliya8@gmail.com>
2026-04-08 18:38:16 +0530

    Create PopularInterests.jsx

---
+++ components/PopularInterests.jsx
@@ -0,0 +1,19 @@
+export default function PopularInterests() {
+  return (
+    <section id="popularInterests" className="section">
+
+      {/* HEADER */}
+      <div className="section-head">
+        <h2 className="section-title">
+          Popular Interests
+        </h2>
+      </div>
+
+      {/* ROW */}
+      <div id="popularRow" className="imdb-row">
+        {/* JS inject karega */}
+      </div>
+
+    </section>
+  );
+}
