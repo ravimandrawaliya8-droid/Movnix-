caf0247b8d328a5f2b7a20309fd13bf40f47a920
kyra-backend <ravimandrawaliya8@gmail.com>
2026-04-08 18:29:32 +0530

    Create DiscoverSection.jsx

---
+++ components/DiscoverSection.jsx
@@ -0,0 +1,21 @@
+export default function DiscoverSection() {
+  return (
+    <section className="section discover">
+
+      {/* HEADER */}
+      <div className="discover-head">
+
+        <h2 className="discover-title">
+          Discover Movies by Genre
+        </h2>
+
+        <a href="/discover" className="discover-link">
+          Action Comedy Drama Movies List
+          <span className="discover-arrow"></span>
+        </a>
+
+      </div>
+
+    </section>
+  );
+}
